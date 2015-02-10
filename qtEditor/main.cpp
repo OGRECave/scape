e80d8e31f6125c7bcada33c57375b3ad40b589e4
jacmoe <jacmoe.dk@gmail.com>
2015-02-10 23:12:33 +0100

    Splash and About

--- qtEditor/main.cpp
+++ qtEditor/main.cpp
@@ -1,15 +1,27 @@
 #include "mainwindow.h"
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QSplashScreen>
 #include "EngineCore/EngineInterface.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    a.setOrganizationName("Scape");
+    a.setApplicationName("qtScape");
+
+    QSplashScreen *splash;
+    QPixmap pixmap(":/images/scape_splash.jpg");
+    splash = new QSplashScreen(pixmap);
+    splash->show();
+
     ScapeEngine::EngineInterface::openConsole();
 
     MainWindow w;
     w.show();
+
+    splash->finish(&w);
+    delete splash;
 
     return a.exec();
 }

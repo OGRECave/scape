1891c299e367c0af986589a6cf6927931939afae
Tobias Schmidt <tobias.schmidt24080878@gmail.com>
2018-08-28 02:49:59 +0200

    Made tool actions checkable

--- qtEditor/mainwindow.cpp
+++ qtEditor/mainwindow.cpp
@@ -92,38 +92,47 @@ void MainWindow::createActions()
     actPencilEraserGPU = new QAction(tr("Pencil/Eraser GPU"), this);
     actPencilEraserGPU->setStatusTip(tr("Pencil/Eraser GPU"));
     actPencilEraserGPU->setIcon(QIcon(":/icons/pencil"));
+    actPencilEraserGPU->setCheckable(true);
 
     actDirectionalNoiseGPU = new QAction(tr("Directional Noise GPU"), this);
     actDirectionalNoiseGPU->setStatusTip(tr("Directional Noise GPU"));
     actDirectionalNoiseGPU->setIcon(QIcon(":/icons/pencil"));
+    actDirectionalNoiseGPU->setCheckable(true);
 
     actNoiseGPU = new QAction(tr("Noise GPU"), this);
     actNoiseGPU->setStatusTip(tr("Noise GPU"));
     actNoiseGPU->setIcon(QIcon(":/icons/pencil"));
+    actNoiseGPU->setCheckable(true);
 
     actSoftenGPU = new QAction(tr("Soften GPU"), this);
     actSoftenGPU->setStatusTip(tr("Soften GPU"));
     actSoftenGPU->setIcon(QIcon(":/icons/pencil"));
+    actSoftenGPU->setCheckable(true);
 
     actSharpenSoften = new QAction(tr("Sharpen/Soften"), this);
     actSharpenSoften->setStatusTip(tr("Sharpen/Soften"));
     actSharpenSoften->setIcon(QIcon(":/icons/smooth"));
+    actSharpenSoften->setCheckable(true);
 
     actNoiseCPU = new QAction(tr("Noise"), this);
     actNoiseCPU->setStatusTip(tr("Noise"));
     actNoiseCPU->setIcon(QIcon(":/icons/noise"));
+    actNoiseCPU->setCheckable(true);
 
     actLevelUnlevel = new QAction(tr("Level/Unlevel"), this);
     actLevelUnlevel->setStatusTip(tr("Level/Unlevel"));
     actLevelUnlevel->setIcon(QIcon(":/icons/level"));
+    actLevelUnlevel->setCheckable(true);
 
     actWaterErosion = new QAction(tr("Water Erosion"), this);
     actWaterErosion->setStatusTip(tr("Water Erosion"));
     actWaterErosion->setIcon(QIcon(":/icons/erosion"));
+    actWaterErosion->setCheckable(true);
 
     actPencilEraser = new QAction(tr("Eraser"), this);
     actPencilEraser->setStatusTip(tr("Eraser"));
     actPencilEraser->setIcon(QIcon(":/icons/ledge"));
+    actPencilEraser->setCheckable(true);
 
     actMaterialProps = new QAction(tr("Material"), this);
     actMaterialProps->setStatusTip(tr("Material Properties"));

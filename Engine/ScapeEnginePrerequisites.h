47e4d1e9c2bcf6896567d16d719a77cebce0c0e7
Tobias Schmidt <tobias.schmidt24080878@gmail.com>
2018-12-27 17:57:44 +0100

    Engine: Moved includes and defines from stdafx.h to ScapeEnginePrerequisites.h

--- Engine/ScapeEnginePrerequisites.h
+++ Engine/ScapeEnginePrerequisites.h
@@ -3,5 +3,24 @@
 
 #include "ScapeEnginePlatform.h"
 
+#include <Ogre.h>
+#include <OgreConfigFile.h>
+#include <OgreException.h>
+#include <OgreStringConverter.h>
+#include <OgreBitwise.h>
+
+#include <string>
+#include <list>
+#include <map>
+#include <vector>
+
+typedef std::string string;
+#ifndef _T
+#define _T(x) x
+#endif
+#ifndef _TCHAR
+#define _TCHAR char
+#endif
+
 #endif // __SCAPEENGINEPREREQUISITES_H__
 

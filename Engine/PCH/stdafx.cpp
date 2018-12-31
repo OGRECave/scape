efca52e66a2aa29911100dc07a431e5c60edeb10
Tobias Schmidt <tobias.schmidt24080878@gmail.com>
2018-12-31 18:02:40 +0100

    Engine: Removed PCH subfolder

--- Engine/PCH/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-/**
- * Giliam de Carpentier, Copyright (c) 2007.
- * Licensed under the Simplified BSD license.
- * See Docs/ScapeLicense.txt for details.
- */
-
-#include "ScapeEngineStableHeaders.h"

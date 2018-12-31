e9f43a1401e362c15546b47c7dc9fb154811847d
Tobias Schmidt <tobias.schmidt24080878@gmail.com>
2018-12-31 17:52:51 +0100

    Engine: Copied includes from stdafx.h to ScapeEngineStableHeaders.h

--- Engine/ScapeEngineStableHeaders.h
+++ Engine/ScapeEngineStableHeaders.h
@@ -1,5 +1,10 @@
 #ifndef __SCAPEENGINESTABLEHEADERS_H__
 #define __SCAPEENGINESTABLEHEADERS_H__
 
+#include "ScapeEnginePrerequisites.h"
+
+#include "Utils/Utils.h"
+#include "EngineCore/EngineCore.h"
+
 #endif // __SCAPEENGINESTABLEHEADERS_H__
 

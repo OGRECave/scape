0ab1fbed85c6e67ac95910b238e7ef3391acb9ac
Tobias Schmidt <tobias.schmidt24080878@gmail.com>
2019-03-17 14:12:09 +0100

    EngineCore: Implemented StartupSettings class

--- Engine/EngineCore/StartupSettings.cpp
+++ Engine/EngineCore/StartupSettings.cpp
@@ -5,7 +5,25 @@
 namespace ScapeEngine
 {
 
-StartupSettings::StartupSettings() {}
+StartupSettings::StartupSettings() : mHeightfieldRows(0), mHeightfieldColumns(0), mHeightfieldHeight(0) {}
 
 StartupSettings::~StartupSettings() {}
+
+int StartupSettings::getHeightfieldRows() const { return mHeightfieldRows; }
+
+void StartupSettings::setHeightfieldRows(int heightfieldRows) { mHeightfieldRows = heightfieldRows; }
+
+int StartupSettings::getHeightfieldColumns() const { return mHeightfieldColumns; }
+
+void StartupSettings::setHeightfieldColumns(int heightfieldColumns)
+{
+    mHeightfieldColumns = heightfieldColumns;
+}
+
+Ogre::Real StartupSettings::getHeightfieldHeight() const { return mHeightfieldHeight; }
+
+void StartupSettings::setHeightfieldHeight(Ogre::Real heightfieldHeight)
+{
+    mHeightfieldHeight = heightfieldHeight;
+}
 }
--- Engine/EngineCore/StartupSettings.h
+++ Engine/EngineCore/StartupSettings.h
@@ -11,6 +11,20 @@ class StartupSettings
 public:
     StartupSettings();
     ~StartupSettings();
+
+    int getHeightfieldRows() const;
+    void setHeightfieldRows(int heightfieldRows);
+
+    int getHeightfieldColumns() const;
+    void setHeightfieldColumns(int heightfieldColumns);
+
+    Ogre::Real getHeightfieldHeight() const;
+    void setHeightfieldHeight(Ogre::Real heightfieldHeight);
+
+protected:
+    int mHeightfieldRows;
+    int mHeightfieldColumns;
+    Ogre::Real mHeightfieldHeight;
 };
 }
 

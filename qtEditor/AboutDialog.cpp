4b6f0b528b7185f26790223ff397326760bc030a
Tobias Schmidt <tobias.schmidt24080878@gmail.com>
2019-09-01 14:41:15 +0200

    qtEditor: Using ScapeVersion.h to determine version number in the about dialog

--- qtEditor/AboutDialog.cpp
+++ qtEditor/AboutDialog.cpp
@@ -1,6 +1,11 @@
 #include "AboutDialog.h"
 #include "ui_aboutdialog.h"
+#include "ScapeVersion.h"
 
-AboutDialog::AboutDialog(QWidget* parent) : QDialog(parent), ui(new Ui::AboutDialog) { ui->setupUi(this); }
+AboutDialog::AboutDialog(QWidget* parent) : QDialog(parent), ui(new Ui::AboutDialog)
+{
+    ui->setupUi(this);
+    ui->titleLabel->setText("Scape " SCAPE_VERSION_NAME);
+}
 
 AboutDialog::~AboutDialog() { delete ui; }
